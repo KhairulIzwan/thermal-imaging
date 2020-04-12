1e597a472dca39bb7481f7c3d059eca6fa00c145
khairulizwan <wansnap@gmail.com>
2020-04-12 21:48:44 +0800

    Updated on 04-12-2020-18:48:44 from 172.20.10.2 : khairulizwan-MacBookPro

--- script/face_detection.py
+++ script/face_detection.py
@@ -74,7 +74,7 @@ class FaceDetector:
 
 			# OTIONAL -- image-rotate """
 			self.cv_image = imutils.rotate(self.cv_image, angle=-90)
-			self.cv_image = cv2.flip(self.cv_image, 1)
+			self.cv_image = cv2.flip(self.cv_image, 0)
 
 			# Clone the original image for displaying purpose later
 			self.frameClone = self.cv_image.copy()
